cbd5caf5fe93ac0da285a79171da15f42ae61970
sunjus <sunju.shin1108@gmail.com>
2021-05-19 16:22:22 +0300

    Added body-parser dependency to handle client info in server

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    trim: true, //이메일주소에 whitespace관리해줌
+    trim: true, //이메일주소에 whitespace관리해 줌
     unique: 1, //이메일주소가 유니크하길 바람
   },
   password: {
@@ -19,13 +19,13 @@ const userSchema = mongoose.Schema({
     maxlength: 50,
   },
   role: {
-    //유저가 관리자인지 아닌지 결정하줌
+    //유저가 관리자인지 아닌지 결정해 줌
     type: Number,
     default: 0,
   },
   image: String,
   token: {
-    //토큰으로 유효성 검사해줌
+    //토큰으로 유효성 검사해 줌
     type: String,
   },
   tokenExp: {
