f643d65a25c43201bc2c4cf963aa3fefb46c04a5
sunjus <sunju.shin1108@gmail.com>
2021-05-19 15:48:28 +0300

    Added user model and schema

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, //이메일주소에 whitespace관리해줌
+    unique: 1, //이메일주소가 유니크하길 바람
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    //유저가 관리자인지 아닌지 결정하줌
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    //토큰으로 유효성 검사해줌
+    type: String,
+  },
+  tokenExp: {
+    //토큰유효기간
+    type: Number,
+  },
+});
+
+const User = mongoose.model("User", userSchema); //()에 모델에 이름과 스키마 넣어주기
+
+module.exports = { User };
